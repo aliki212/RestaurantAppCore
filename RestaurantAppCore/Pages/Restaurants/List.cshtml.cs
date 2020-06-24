745dba1e34fb4c2ba857693bb76d8b9044f725ac
aliki212 <aliki212@gmail.com>
2020-06-24 22:18:14 +0300

    First Razor Page - List - use a field string "Message" to call forward the message - or an IConfiguration configuration using Dependency Injection to bring a message from the appsettings.json custom property - here : "Mess".

---
+++ RestaurantAppCore/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace RestaurantAppCore.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public ListModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = _configuration["Mess"];
+        }
+    }
+}
